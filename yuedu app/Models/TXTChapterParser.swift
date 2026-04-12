07b2accda7afe62f3b88f160eb4ae9514cda1694
CHANG-JUI-LIN <r3212239269@gmail.com>
2026-04-13 00:55:21 +0800

    debug: log titleMatches count in parseMappedChapterIndexes

--- yuedu app/Models/TXTChapterParser.swift
+++ yuedu app/Models/TXTChapterParser.swift
@@ -108,6 +108,7 @@ enum TXTChapterParser {
         }
 
         let titleMatches = detectMappedTitleMatches(in: mappedTextFile)
+        print("[TXTChapterParser] titleMatches count=\(titleMatches.count), titles=\(titleMatches.prefix(5).map(\.title))")
         if !titleMatches.isEmpty {
             var indexes: [TXTMappedChapterIndex] = []
 
