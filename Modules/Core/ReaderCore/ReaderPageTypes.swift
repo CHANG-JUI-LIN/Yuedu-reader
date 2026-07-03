5f8338ed140b2944f7cc126da7c4dffb34a249d2
CHANG-JUI-LIN <r3212239269@gmail.com>
2026-07-03 12:26:36 +0800

    refactor: relocate Reader UI types from Core to Features layer
    - git mv AutoReadController, FootnotePopover, ReaderPagingAdapter→ReaderCoverPageMotion
    - Extract 3 PreferenceKey into ReaderPreferenceKeys.swift
    - Remove unused import SwiftUI from ReaderPageTypes.swift

--- Modules/Core/ReaderCore/ReaderPageTypes.swift
+++ Modules/Core/ReaderCore/ReaderPageTypes.swift
@@ -1,4 +1,4 @@
-import SwiftUI
+import Foundation
 
 struct PageContent {
     let chapterIndex: Int
@@ -6,23 +6,6 @@ struct PageContent {
     let content: String
     let pageInChapter: Int
     var attributedContent: NSAttributedString?
-}
-
-struct ReaderSafeAreaTopKey: PreferenceKey {
-    static var defaultValue: CGFloat { 0 }
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) { value = nextValue() }
-}
-
-struct ReaderViewportSizeKey: PreferenceKey {
-    static var defaultValue: CGSize { .zero }
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) { value = nextValue() }
-}
-
-struct EpubVerticalPageOffsetKey: PreferenceKey {
-    static var defaultValue: [Int: CGFloat] { [:] }
-    static func reduce(value: inout [Int: CGFloat], nextValue: () -> [Int: CGFloat]) {
-        value.merge(nextValue(), uniquingKeysWith: { _, new in new })
-    }
 }
 
 enum PageTurnAnimation {
---
+++ Modules/Features/Reader/ReaderPreferenceKeys.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ReaderSafeAreaTopKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) { value = nextValue() }
+}
+
+struct ReaderViewportSizeKey: PreferenceKey {
+    static var defaultValue: CGSize { .zero }
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) { value = nextValue() }
+}
+
+struct EpubVerticalPageOffsetKey: PreferenceKey {
+    static var defaultValue: [Int: CGFloat] { [:] }
+    static func reduce(value: inout [Int: CGFloat], nextValue: () -> [Int: CGFloat]) {
+        value.merge(nextValue(), uniquingKeysWith: { _, new in new })
+    }
+}
